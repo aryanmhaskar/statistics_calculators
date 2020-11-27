e1ead8b5f64975eee97a8888c965ae652c12bfa6
aryanmhaskar <aryanmhaskar@hotmail.com>
2020-11-26 22:07:00 -0500

    added naming data sets and formatting output

--- desc_stat_calc.py
+++ desc_stat_calc.py
@@ -3,6 +3,7 @@ from sigfig import round
 
 def create_data_set():
     data_set = [] # Set a list to hold all data points for any given scenario
+    data_set_name = input("Name of data set:") # Allow user to name each data set
     while True:
         string_value = input("Enter single data value then hit enter\n When all data points entered, type 'quit': ") # Ask for each individual data point
         if string_value == 'quit': # Add way to break from loop after all data points entered
@@ -14,18 +15,19 @@ def create_data_set():
                 print(f"Current data set:\n {data_set}") # Allow user to see data values entered.
             except ValueError:
                 print("Enter number or 'quit'") # In case user doesn't enter 'quit' or a number
+    data_set.append(data_set_name) # Make the name of the data set the last item in the list
     return data_set
 
 while True:
     try:
-        string_sets = input("Number of data sets: ")
+        string_sets = input("Number of data sets: ") # Ask for the number of data sets to be processed
         number_of_data_sets = int(string_sets) 
         break
     except ValueError:
         print("Enter an integer")    
 
 data_sets = []
-for x in range(0, number_of_data_sets):
+for x in range(0, number_of_data_sets): # Create a 2-dimensional list that holds all data sets
     data_sets.append(create_data_set())
 
 while True:
@@ -36,27 +38,29 @@ while True:
     except ValueError: # Prevent ValueError convert the String input to an integer from occuring
         print("Enter an integer")
 
-for data_set in data_sets:
+mean_list = [] # Add all means to this number to perform a t-test
+for data_set in data_sets: # Do statistical analysis for each data set
     mean_sum = 0 
-    for value in data_set: # Add all the values to create the sum to calculate mean
+    for value in data_set[:-1]: # Add all the values to create the sum to calculate mean, remove last because last is the name
         mean_sum += value 
 
-    mean = round(mean_sum/len(data_set), sigfigs=sig_figs) # Calculate the mean with significant figures
+    mean = round(mean_sum/(len(data_set) - 1), sigfigs=sig_figs) # Calculate the mean with significant figures, subtract because last element is name
+    mean_list.append(mean)
 
     minimum = data_set[0]
-    for value in data_set: # Find the maximum for the range
+    for value in data_set[:-1]: # Find the maximum for the range subtract because last element is name
         if value < minimum:
             minimum = value
 
     maximum = data_set[0]
-    for value in data_set: # Find the minimum for the range
+    for value in data_set[:-1]: # Find the minimum for the range subtract because last element is name
         if value > maximum:
             maximum = value
 
     range = maximum-minimum # Calculate the range
 
     squared_values = [] 
-    for value in data_set: # Subtract each value by the mean and square the result, then add to list.
+    for value in data_set[:-1]: # Subtract each value by the mean and square the result, then add to list subtract because last element is name
         sqvalue = (value-mean)**2
         squared_values.append(sqvalue)
 
@@ -64,7 +68,7 @@ for data_set in data_sets:
     for value in squared_values: # Add all squared values to be used to calculate the variance and standard deviation
         squared_sum += value
 
-    variance = float(squared_sum/(len(data_set) - 1)) # Calculate the variance
+    variance = float(squared_sum/(len(data_set) - 2)) # Calculate the variance, subtract because last element is name, subtract another because formula is n-1
     standard_deviation = math.sqrt(variance) # Calculate the standard deviation
 
     # Calculate all the SD ranges, L means lower boud, H means upper bound. 
@@ -75,18 +79,24 @@ for data_set in data_sets:
     L3sd = mean-(3*standard_deviation)
     H3sd = mean+(3*standard_deviation)
 
-
     # Output all the values for a descriptive statistics table
-    output = f"""
-    mean: {mean}\n
-    range: {range}\n
-    maximum: {maximum}\n
-    minimum: {minimum}\n
-    variance: {variance}\n
-    standard deviation: {standard_deviation}\n
+    # The data_set[-1] is to access the name of the data set
+    output = f"""___________________________________
+{data_set[-1]}:
+mean: {mean}
+
+range: {range}
+    maximum: {maximum}
+    minimum: {minimum}
+
+variance: {variance}
+standard deviation: {standard_deviation}
     1SD: {L1sd}-{H1sd}
     2SD: {L2sd}-{H2sd}
     3SD: {L3sd}-{H3sd}
-    Number: {len(data_set)}
+
+Number: {len(data_set) -1} 
+___________________________________
     """
     print(output)
+
